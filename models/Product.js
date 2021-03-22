609d92961a01d4950aaed3d09b948b5c001e5c53
Cameron Harvey <charvey0@gmail.com>
2021-03-21 22:23:52 -0700

    Defined the columns for Category, Product, ProductTag and Tag models

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,11 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
+const {
+  validate
+} = require('../config/connection');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -7,17 +13,42 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
-    // define columns
+Product.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+    allowNull: false,
   },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10;
+    validate: {
+      inInt: true,
+    },
   }
-);
+  category_id: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'product',
+});
 
 module.exports = Product;

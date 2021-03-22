aaef9738592b7ad08a182297690ad2a0b5df9f00
Cameron Harvey <charvey0@gmail.com>
2021-03-21 22:44:13 -0700

    Made associations between the tables in models/index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  foreignKey: 'id',
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  foreignKey: 'id',
+});
 
 module.exports = {
   Product,
